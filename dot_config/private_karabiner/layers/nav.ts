6b72836bbffaa071f3af9f520ccb1af0b376f5b0
Brice Carpentier <brice.carpentier@mistral.ai>
2026-01-21 18:14:46 +0100

    Add vim-like navigation commands (w, b, ^, $)

--- dot_config/private_karabiner/layers/nav.ts
+++ dot_config/private_karabiner/layers/nav.ts
@@ -8,6 +8,12 @@ export default (): RuleBuilder =>
 			map("k", [], ["option", "command"]).to("up_arrow"),
 			map("l", [], ["option", "command"]).to("right_arrow"),
 
+			map("4", [], ["shift"]).to("e", ["control"]),
+			map("6", [], ["shift"]).to("a", ["control"]),
+
+			map("w", [], ["shift"]).to("right_arrow", ["option"]),
+			map("b", [], ["shift"]).to("left_arrow", ["option"]),
+
 			map("j", ["left_shift"]).to("page_down"),
 			map("k", ["left_shift"]).to("page_up"),
 		])
